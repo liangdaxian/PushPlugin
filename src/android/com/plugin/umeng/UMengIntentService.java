9626434c24f1c4e6baff29252e52467311f31672
jiliang <liangji101@yahoo.cn>
2015-02-03 20:52:23 +0800

    update java

--- src/android/com/plugin/umeng/UMengIntentService.java
+++ src/android/com/plugin/umeng/UMengIntentService.java
@@ -15,65 +15,64 @@ import android.util.Log;
 
 import org.android.agoo.client.BaseConstants;
 
-import com.umeng.common.message.Log;
 import com.umeng.message.UmengBaseIntentService;
 import com.umeng.message.entity.UMessage;
 
 @SuppressLint("NewApi")
 public class UMengIntentService extends UmengBaseIntentService {
 
-	private static final String TAG = "UMengIntentService";
-	
-	public UMengIntentService() {
-		super("UMengIntentService");
-	}
+    private static final String TAG = "UMengIntentService";
 
-	@Override
-	public void onRegistered(Context context, String regId) {
+    public UMengIntentService() {
+        super();
+    }
 
-		Log.v(TAG, "onRegistered: "+ regId);
+    @Override
+    public void onRegistered(Context context, String regId) {
 
-		JSONObject json;
+        Log.v(TAG, "onRegistered: "+ regId);
 
-		try
-		{
-			json = new JSONObject().put("event", "registered");
-			json.put("regid", regId);
+        JSONObject json;
 
-			Log.v(TAG, "onRegistered: " + json.toString());
+        try
+        {
+            json = new JSONObject().put("event", "registered");
+            json.put("regid", regId);
 
-			// Send this JSON data to the JavaScript application above EVENT should be set to the msg type
-			// In this case this is the registration ID
-			PushPlugin.sendJavascript( json );
+            Log.v(TAG, "onRegistered: " + json.toString());
 
-		}
-		catch( JSONException e)
-		{
-			// No message to the user is sent, JSON failed
-			Log.e(TAG, "onRegistered: JSON exception");
-		}
-	}
+            // Send this JSON data to the JavaScript application above EVENT should be set to the msg type
+            // In this case this is the registration ID
+            PushPlugin.sendJavascript( json );
 
-	@Override
-	public void onUnregistered(Context context, String regId) {
-		Log.d(TAG, "onUnregistered - regId: " + regId);
-	}
+        }
+        catch( JSONException e)
+        {
+            // No message to the user is sent, JSON failed
+            Log.e(TAG, "onRegistered: JSON exception");
+        }
+    }
 
-	@Override
-	protected void onMessage(Context context, Intent intent) {
-		Log.d(TAG, "onMessage - context: " + context);
+    @Override
+    public void onUnregistered(Context context, String regId) {
+        Log.d(TAG, "onUnregistered - regId: " + regId);
+    }
 
-		// Extract the payload from the message
-		Bundle extras = intent.getExtras();
-		if (extras != null)
-		{
-			// if we are in the foreground, just surface the payload, else post it to the statusbar
+    @Override
+    protected void onMessage(Context context, Intent intent) {
+        Log.d(TAG, "onMessage - context: " + context);
+
+        // Extract the payload from the message
+        Bundle extras = intent.getExtras();
+        if (extras != null)
+        {
+            // if we are in the foreground, just surface the payload, else post it to the statusbar
             if (PushPlugin.isInForeground()) {
-				extras.putBoolean("foreground", true);
+                extras.putBoolean("foreground", true);
                 PushPlugin.sendExtras(extras);
-			}
-			else {
-				extras.putBoolean("foreground", false);
+            }
+            else {
+                extras.putBoolean("foreground", false);
 
                 // Send a notification if there is a message
                 if (extras.getString("message") != null && extras.getString("message").length() != 0) {
@@ -81,77 +80,77 @@ public class UMengIntentService extends UmengBaseIntentService {
                 }
             }
         }
-	}
+    }
 
-	public void createNotification(Context context, Bundle extras)
-	{
-		NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-		String appName = getAppName(this);
+    public void createNotification(Context context, Bundle extras)
+    {
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        String appName = getAppName(this);
 
-		Intent notificationIntent = new Intent(this, PushHandlerActivity.class);
-		notificationIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-		notificationIntent.putExtra("pushBundle", extras);
+        Intent notificationIntent = new Intent(this, PushHandlerActivity.class);
+        notificationIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        notificationIntent.putExtra("pushBundle", extras);
 
-		PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-		
-		int defaults = Notification.DEFAULT_ALL;
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
 
-		if (extras.getString("defaults") != null) {
-			try {
-				defaults = Integer.parseInt(extras.getString("defaults"));
-			} catch (NumberFormatException e) {}
-		}
-		
-		NotificationCompat.Builder mBuilder =
-			new NotificationCompat.Builder(context)
-				.setDefaults(defaults)
-				.setSmallIcon(context.getApplicationInfo().icon)
-				.setWhen(System.currentTimeMillis())
-				.setContentTitle(extras.getString("title"))
-				.setTicker(extras.getString("title"))
-				.setContentIntent(contentIntent)
-				.setAutoCancel(true);
+        int defaults = Notification.DEFAULT_ALL;
 
-		String message = extras.getString("message");
-		if (message != null) {
-			mBuilder.setContentText(message);
-		} else {
-			mBuilder.setContentText("<missing message content>");
-		}
+        if (extras.getString("defaults") != null) {
+            try {
+                defaults = Integer.parseInt(extras.getString("defaults"));
+            } catch (NumberFormatException e) {}
+        }
 
-		String msgcnt = extras.getString("msgcnt");
-		if (msgcnt != null) {
-			mBuilder.setNumber(Integer.parseInt(msgcnt));
-		}
-		
-		int notId = 0;
-		
-		try {
-			notId = Integer.parseInt(extras.getString("notId"));
-		}
-		catch(NumberFormatException e) {
-			Log.e(TAG, "Number format exception - Error parsing Notification ID: " + e.getMessage());
-		}
-		catch(Exception e) {
-			Log.e(TAG, "Number format exception - Error parsing Notification ID" + e.getMessage());
-		}
-		
-		mNotificationManager.notify((String) appName, notId, mBuilder.build());
-	}
-	
-	private static String getAppName(Context context)
-	{
-		CharSequence appName = 
-				context
-					.getPackageManager()
-					.getApplicationLabel(context.getApplicationInfo());
-		
-		return (String)appName;
-	}
-	
-	@Override
-	public void onError(Context context, String errorId) {
-		Log.e(TAG, "onError - errorId: " + errorId);
-	}
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(context)
+                        .setDefaults(defaults)
+                        .setSmallIcon(context.getApplicationInfo().icon)
+                        .setWhen(System.currentTimeMillis())
+                        .setContentTitle(extras.getString("title"))
+                        .setTicker(extras.getString("title"))
+                        .setContentIntent(contentIntent)
+                        .setAutoCancel(true);
+
+        String message = extras.getString("message");
+        if (message != null) {
+            mBuilder.setContentText(message);
+        } else {
+            mBuilder.setContentText("<missing message content>");
+        }
+
+        String msgcnt = extras.getString("msgcnt");
+        if (msgcnt != null) {
+            mBuilder.setNumber(Integer.parseInt(msgcnt));
+        }
+
+        int notId = 0;
+
+        try {
+            notId = Integer.parseInt(extras.getString("notId"));
+        }
+        catch(NumberFormatException e) {
+            Log.e(TAG, "Number format exception - Error parsing Notification ID: " + e.getMessage());
+        }
+        catch(Exception e) {
+            Log.e(TAG, "Number format exception - Error parsing Notification ID" + e.getMessage());
+        }
+
+        mNotificationManager.notify((String) appName, notId, mBuilder.build());
+    }
+
+    private static String getAppName(Context context)
+    {
+        CharSequence appName =
+                context
+                        .getPackageManager()
+                        .getApplicationLabel(context.getApplicationInfo());
+
+        return (String)appName;
+    }
+
+    @Override
+    public void onError(Context context, String errorId) {
+        Log.e(TAG, "onError - errorId: " + errorId);
+    }
 
 }
